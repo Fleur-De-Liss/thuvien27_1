190376a0b85f5f13a8a71481b9ec4b65340279b0
Fleur-De-Liss <tsundere199@gmail.com>
2020-08-24 22:15:42 +0700

    qlthuvien_1

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "b'ZZ~\xa6T\xf7\xe9\x9e\xd6oo\x8e\xaaq!O'"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost/qltv_test?charset=utf8mb4"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
+
+admin = Admin(app=app, name="Quản lý thư viện", template_mode="bootstrap3")
